a0d0be3cd7f90340063292d5d11cfee5055d07e1
pierrelng <langerreip@gmail.com>
2017-07-29 18:35:53 +0200

    Display events on homepage, ordered by start_time

--- wp-content/themes/twentyfifteen-child/functions.php
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'twentyfifteen_child_enqueue_styles' );
 function twentyfifteen_child_enqueue_styles() {
     $parent_style = 'twentyfifteen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
@@ -8,4 +9,14 @@ function twentyfifteen_child_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+}
+
+add_action( 'pre_get_posts', 'change_home_post_type' );
+function change_home_post_type( $query ) {
+    if ( $query->is_main_query() && $query->is_home() ) {
+        $query->set( 'post_type', array( 'post', 'events' ) );
+        $query->set( 'meta_key', 'start_time' );
+		$query->set( 'orderby', 'meta_value' );
+		$query->set( 'order', 'ASC' );
+    }
 }
