cc8358ae87f12764ab87157c110b8b546e7e9aa2
pierrelng <langerreip@gmail.com>
2017-08-11 11:28:00 +0200

    Move action to display events to a custom plugin

--- wp-content/themes/twentyfifteen-child/functions.php
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -10,13 +10,3 @@ function twentyfifteen_child_enqueue_styles() {
         wp_get_theme()->get('Version')
     );
 }
-
-add_action( 'pre_get_posts', 'change_home_post_type' );
-function change_home_post_type( $query ) {
-    if ( $query->is_main_query() && $query->is_home() ) {
-        $query->set( 'post_type', array( 'post', 'events' ) );
-        $query->set( 'meta_key', 'start_time' );
-		$query->set( 'orderby', 'meta_value' );
-		$query->set( 'order', 'ASC' );
-    }
-}
